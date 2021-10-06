f18554025ce530beee7e5869b8087fdb35529e4e
Lucas <lucasalonso11800@gmail.com>
2021-10-06 16:48:34 -0300

    Added dotenv

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
 import faker from 'faker';
+import 'dotenv/config';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -67,5 +67,5 @@ app.post('/populateDB', (req, res) => {
     res.json('Everything is working');
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = 5002 || process.env.PORT;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
