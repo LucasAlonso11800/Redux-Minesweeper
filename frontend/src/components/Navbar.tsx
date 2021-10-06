ef89c48fbe3b882e2c9ff6b5e90b72569118265b
Lucas <lucasalonso11800@gmail.com>
2021-10-06 16:32:39 -0300

    Style fixes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { AppBar, IconButton, Toolbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import { Link } from 'react-router-dom'
 
+const useStyles = makeStyles(() => ({
+    link: {
+        textShadow: '2px 2px 5px #000',
+        textDecoration: 'none',
+        color: '#FFF'
+    }
+}));
+
 export default function Navbar() {
+    const classes = useStyles()
     return (
         <AppBar position="relative" color="transparent">
             <Toolbar>
-                <IconButton><Link to="/">Play</Link></IconButton>
-                <IconButton><Link to="/scores">Check best scores</Link></IconButton>
+                <IconButton><Link className={classes.link} to="/">Play</Link></IconButton>
+                <IconButton><Link className={classes.link} to="/scores">Check best scores</Link></IconButton>
             </Toolbar>
         </AppBar>
     )
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,15 +3,26 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as HomePageActionCreators from '../state/Home/HomePage.actionCreators';
+// styles
+import { makeStyles } from '@material-ui/styles';
 // Components
 import Tile from '../components/Tile';
 import { Button, FormControl, FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import SubmitModal from '../components/SubmitModal';
 // Types
 import { State } from '../state/RootReducer';
 // Helpers
 import { repeat } from '../helpers/Helpers';
 import { EGameStatus } from '../types/types';
-import SubmitModal from '../components/SubmitModal';
+
+const useStyles = makeStyles(() => ({
+    button: {
+        fontFamily: 'Secular One, sans-serif'
+    },
+    radio: {
+        color: '#FFF'
+    }
+}));
 
 export default function HomePage() {
     const [difficulty, setDifficulty] = useState(1);
@@ -25,6 +36,8 @@ export default function HomePage() {
         setGameWaiting()
     }, []);
 
+    const classes = useStyles();
+
     return (
         <main className="container">
             <div className="info">
@@ -37,11 +50,29 @@ export default function HomePage() {
                     <div className="form-input">
                         <FormControl component="fieldset">
                             <RadioGroup value={difficulty} onChange={(e) => setDifficulty(parseInt(e.currentTarget.value))} >
-                                <FormControlLabel value={1} control={<Radio />} label="Easy" disabled={gameStatus === EGameStatus.PLAYING} />
-                                <FormControlLabel value={2} control={<Radio />} label="Medium" disabled={gameStatus === EGameStatus.PLAYING} />
-                                <FormControlLabel value={3} control={<Radio />} label="Hard" disabled={gameStatus === EGameStatus.PLAYING} />
+                                <FormControlLabel
+                                    value={1}
+                                    control={<Radio color="default" className={classes.radio} />}
+                                    label="Easy"
+                                    disabled={gameStatus === EGameStatus.PLAYING}
+                                />
+                                <FormControlLabel
+                                    value={2}
+                                    control={<Radio color="default" className={classes.radio} />}
+                                    label="Medium"
+                                    disabled={gameStatus === EGameStatus.PLAYING}
+                                />
+                                <FormControlLabel
+                                    value={3}
+                                    control={<Radio color="default" className={classes.radio} />}
+                                    label="Hard"
+                                    disabled={gameStatus === EGameStatus.PLAYING}
+                                />
                             </RadioGroup>
-                            <Button type="button" variant="contained"
+                            <Button
+                                type="button"
+                                variant="contained"
+                                className={classes.button}
                                 disabled={gameStatus === EGameStatus.PLAYING}
                                 onClick={() => startGame(difficulty)}>
                                 Start game
